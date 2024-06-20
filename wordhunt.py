c294434f4cd1f61ad3775356a1a5ea44db8f397b
Jeffrey Shen <sunnyjeff0606@gmail.com>
2024-06-20 13:06:42 -0400

    v2 added main

--- wordhunt.py
+++ wordhunt.py
@@ -1,29 +1,30 @@
-from trie import Node, Trie
-from board import Board
-#change size here
-sidelength = 4
+def main():
+    from trie import Node, Trie
+    from board import Board
+    #change size here
+    sidelength = 4
 
-#opens file
-words = open("newdict.txt", "rt")
+    #opens file
+    words = open("newdict.txt", "rt")
 
-#creates new trie
-trie = Trie()
+    #creates new trie
+    trie = Trie()
 
-for x in words:
-    word = x.strip().lower()
-    trie.insert(word)
+    for x in words:
+        word = x.strip().lower()
+        trie.insert(word)
 
-#creates new Board
-letters = input("Insert Word Hunt Board Here")
-board = Board()
-board.createBoard(letters, sidelength)
-final = board.findallpath(trie)
+    #creates new Board
+    letters = input("Insert Word Hunt Board Here")
+    board = Board()
+    board.createBoard(letters, sidelength)
+    final = board.findallpath(trie)
 
-#sorting final
-final = sorted(final, key=len, reverse=False)
-for x in final:
-    print(x)
+    #sorting final
+    final = sorted(final, key=len, reverse=False)
+    for x in final:
+        print(x)
 
-
+main()
 
 
